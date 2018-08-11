ce569457611e19549d01e653addcf4595ad6780d
maxim <maxim@exemple.ru>
2018-08-12 00:31:45 +0300

    Изменены 3 файла

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ function result() {
 	n2 = parseFloat(document.getElementById('n2').value);
 
 	if (isNaN(n1) || isNaN(n2)) {
-	  alert('Введите число!');
-	  return;
+		alert('Введите число!');
+		return;
 	}
 
 	sel = document.getElementById('sel').value;
 
 	switch (sel) {
-	  case '+':
-	    res = n1 + n2;
-	    break;
-	  case '-':
-	    res = n1 - n2;
-	    break;
-	  case '*':
-	    res = n1 * n2;
-	    break;
-	  case '/':
-	    if (n2 === 0) {
-		alert('Делить на 0 нельзя!');
-		return;
-	    }		
-	    res = n1 / n2;
-	    break;
+		case '+':
+		    res = n1 + n2;
+		    break;
+		case '-':
+		    res = n1 - n2;
+		    break;
+		case '*':
+		    res = n1 * n2;
+		    break;
+		case '/':
+		    if (n2 === 0) {
+				alert('Делить на 0 нельзя!');
+				return;
+		    }		
+		    res = n1 / n2;
+		    break;
 	}
 
 	document.getElementById('res').innerHTML = res;
